40bdc7f451f8c2946b7d814af97f5498a9789ba4
Taylor <taylor.ernst@hotmail.com>
2019-04-04 17:57:13 -0700

    Fixed random num generator and added a header. Added the srand seed to the beginning of main.

--- randomGenerator.cpp
+++ randomGenerator.cpp
@@ -1,11 +1,10 @@
 
-#include <time.h>
 #include <cstdlib>
 
-int randomGenerator(int toNum)	{
-	srand(time(NULL));
-	int aRand= rand() % (toNum + 1);
-	return aRand;
+// Returns a random number bewteen 0 and maxValue (default = 255).
+int randomGenerator(int maxValue = 255)
+{
+    int randomNum = rand() % (maxValue + 1);
+    return randomNum;
 }
-
 
