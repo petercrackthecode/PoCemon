45fa5e1fba86dde66f528652dacbd90905fc8205
Taylor <taylor.ernst@hotmail.com>
2019-04-08 17:47:44 -0700

    Testing damage and attacks.

--- src/testTay.cpp
+++ src/testTay.cpp
@@ -1,8 +1,26 @@
 #include <iostream>
+#include "PoCemon.h"
+#include "AttackStd.h"
 
 using namespace std;
 
 void tay()
 {
-    cout << "nice.";
+    AttackStd attack(2, "Kate", "Hi there", Type::Normal, AbilityCategory::Physical, 3, 4, 5);
+
+    Pocemon testAttacker(PkmnId::Abra, 99);
+    Pocemon testDefender(PkmnId::Abra, 99);
+
+    AttackStd megaPunch(5, "Mega Punch", "", Type::Normal, AbilityCategory::Physical, 80, 216, 20);
+    //for (int i = 0; i < 20; ++i)
+    megaPunch.mainEffect(testAttacker, testDefender);
+
+    //Person.displayInfo();
+    //testAttacker.doDamage(attack, Person);
+
+    cin.get();
+
+    testDefender.displayInfo();
+
+    cin.get();
 }
