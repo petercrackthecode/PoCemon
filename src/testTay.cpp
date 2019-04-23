3fb54dc55a95a01eccb14fc7b2299806e453e522
Taylor <taylor.ernst@hotmail.com>
2019-04-23 16:47:55 -0700

    Created functions to generate a random team and get the currently active PoCemon from a team.

--- src/testTay.cpp
+++ src/testTay.cpp
@@ -2,6 +2,7 @@
 #include "PoCemon.h"
 #include "AttackAbility.h"
 #include "Player.h"
+#include "randomGenerator.h"
 
 using namespace std;
 
@@ -11,29 +12,50 @@ void tay()
 
     AttackAbility attack(2, "Kate", "Hi there", Type::Normal, AbilityCategory::Physical, 3, 4, 5, 6);
 
-    Pocemon testAttacker(PkmnId::Aerodactyl, 99);
-    Pocemon testDefender(PkmnId::Abra, 99);
+    //Pocemon testAttacker(PkmnId::Aerodactyl, 99);
+    //Pocemon testDefender(PkmnId::Abra, 99);
 
     AttackAbility megaPunch(5, "Mega Punch", "", Type::Normal, AbilityCategory::Physical, 80, 216, 20, 0);
 
-    Player t("tay");
-    t.addToTeam(testAttacker);
-    t.addToTeam(testDefender);
-
-    t.displayTeam();
+    Player player1("tay");
+    player1.generateRandomTeam();
+    player1.displayTeam();
     cin.get();
 
+    Player player2("other");
+    player2.generateRandomTeam();
+    
+    player2.displayTeam();
+    cin.get();
+
+    cout << "...\n...\n";
+
+    Pocemon *p1Active = player1.getActivePocemon();
+    p1Active->displayInfo();
+
+    Pocemon *p2Active = player2.getActivePocemon();
+    p2Active->displayInfo();
+
+    cout << "...\n...\n";
 
 
     //for (int i = 0; i < 20; ++i)
-    megaPunch.use(testAttacker, testDefender);
+    while (player1.activePocemonIndex < 6) {
+        p1Active = player1.getActivePocemon();
+        p2Active = player2.getActivePocemon();
+        while (p2Active->getCurHp() > 0)
+            megaPunch.use(*p1Active, *p2Active);
+        player1.activePocemonIndex++;
+        player2.activePocemonIndex++;
+    }
 
     //Person.displayInfo();
     //testAttacker.doDamage(attack, Person);
 
     cin.get();
 
-    testDefender.displayInfo();
+    player1.displayTeam();
+
 
     cin.get();
 }
