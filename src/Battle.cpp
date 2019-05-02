99712ac0a32afd89777f3465fee14a48611c7418
Taylor <taylor.ernst@hotmail.com>
2019-05-01 18:15:26 -0700

    Added flow control methods to Battle class.

--- src/Battle.cpp
+++ src/Battle.cpp
@@ -1,3 +1,4 @@
+#include <iostream>
 #include <string>
 #include "Battle.h"
 #include "Structs.h"
@@ -21,6 +22,9 @@ void Battle::addEvent(BattleEvent evt)
 
 BattleEvent Battle::peekNextEvent()
 {
+    if (battleEvents.empty())
+        throw "No events pending"; // TODO: Class exception.
+
     BattleEvent evt = battleEvents.front();
     return evt;
 }
@@ -28,7 +32,73 @@ BattleEvent Battle::peekNextEvent()
 
 BattleEvent Battle::extractNextEvent()
 {
+    if (battleEvents.empty())
+        throw "No events pending"; // TODO: Class exception.
+
     BattleEvent evt = battleEvents.front();
     battleEvents.pop();
     return evt;
+}
+
+
+bool Battle::eventPending() const
+{
+    if (battleEvents.empty())
+        return false;
+    else
+        return true;
+}
+
+
+void Battle::setP1Action(int i)
+{
+    if (p1Action == -1)
+    {
+        p1Action = i;
+        ++pendingActions;
+    }
+};
+
+void Battle::setP2Action(int i)
+{
+    if (p2Action == -1)
+    {
+        p2Action = i;
+        ++pendingActions;
+    }
+};
+
+
+void Battle::setAttackingPriority()
+{
+    int p1Speed = p1.getActivePocemon()->getCurSpeed();
+    int p2Speed = p2.getActivePocemon()->getCurSpeed();
+
+    if (p2Speed > p1Speed)
+    {
+        attackingPlayer = &p2;
+        defendingPlayer = &p1;
+    }
+    else
+    {
+        attackingPlayer = &p1;
+        defendingPlayer = &p2;
+    }
+}
+
+
+void Battle::switchPlayerRoles()
+{
+    if (&p1 == attackingPlayer)
+    {
+        cout << "switching p1 to defend\n";
+        attackingPlayer = &p2;
+        defendingPlayer = &p1;
+    } else
+    {
+        cout << "switching p1 to attack\n";
+        attackingPlayer = &p1;
+        defendingPlayer = &p2;
+    }
+    --pendingActions;
 }
--- src/Battle.h
+++ src/Battle.h
@@ -12,16 +12,39 @@ public:
     Battle(const std::string &p1Name, const std::string &p2Name);
 
     bool next();
-    static void addEvent(BattleEvent evt);;
+    static void addEvent(BattleEvent evt);
     BattleEvent peekNextEvent();
-    BattleEvent extractNextEvent()
+    BattleEvent extractNextEvent();
+    bool eventPending() const;
+
+    bool canContinue() const { return canContinueFlag; };
+    void setContinue() { canContinueFlag = true; };
+    void setPause() { canContinueFlag = false; };
+
+    int getAttackerAction() { return (&p1 == attackingPlayer) ? p1Action : p2Action; };
+    int getDefenderAction() { return (&p1 == defendingPlayer) ? p1Action : p2Action; };
+    void setP1Action(int i);
+    void setP2Action(int i);
+    bool playersReady() { return (pendingActions == 2) ? true : false; };
+    void resetPlayerActions() { p1Action = -1; p2Action = -1; };
+    bool actionsPending() const { return (pendingActions > 0) ? true : false; }
+
+    void setAttackingPriority();
+    void switchPlayerRoles();
+
+    Player* getAttackingPlayer() { return attackingPlayer; };
+    Player* getDefendingPlayer() { return defendingPlayer; };
 
 private:
+    bool canContinueFlag = true;
     Player &p1;
     Player &p2;
     Player *attackingPlayer = nullptr;
     Player *defendingPlayer = nullptr;
     static std::queue<BattleEvent> battleEvents;
+    int pendingActions = 0;
+    int p1Action = -1;
+    int p2Action = -1;
 
     // Probably getting rid of this:
     //enum class State
